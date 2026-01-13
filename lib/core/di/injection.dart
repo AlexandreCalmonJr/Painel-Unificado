5cc393b8666c911ab95358a5c87c24e519b02b20
AlexandreCalmon <alexandrecalmonjunior@gmail.com>
2026-01-13 20:23:57 -0300

    feat: set up dependency injection with GetIt and Injectable.

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -2,21 +2,8 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:painel_windowns/core/di/injection.config.dart';
 
-/// Global service locator instance.
 final getIt = GetIt.instance;
 
-/// Configures all dependencies using Injectable.
-///
-/// This function must be called before using any dependencies.
-/// Typically called in main() before runApp().
-///
-/// Example:
-/// ```dart
-/// void main() async {
-///   WidgetsFlutterBinding.ensureInitialized();
-///   await configureDependencies();
-///   runApp(MyApp());
-/// }
-/// ```
+
 @InjectableInit()
 Future<void> configureDependencies() async => getIt.init();
